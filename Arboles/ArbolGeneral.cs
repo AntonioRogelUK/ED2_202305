e28298ec79cdedd711df4233e8c9582e4846bcfc
Jesus Noriega <jesusadan049@gmail.com>
2023-06-10 09:00:55 -0700

    Insertando Nodos

--- Arboles/ArbolGeneral.cs
+++ Arboles/ArbolGeneral.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,34 @@ namespace Arboles
     {
         private readonly Nodo raiz;
         public Nodo Raiz { get { return raiz; } }
+
         public ArbolGeneral(string dato)
         {
             raiz = new Nodo(dato);
+        }
+
+        public Nodo InsertarNodo(string dato, Nodo nodoPadre)
+        {
+            if (nodoPadre == null)
+            {
+                throw new Exception("No se a especificado padre");
+            }
+
+            if(nodoPadre.Hijo == null)
+            {
+                nodoPadre.Hijo = new Nodo(dato);
+                return nodoPadre.Hijo;
+            }
+            else
+            {
+                Nodo nodoActual = nodoPadre.Hijo;
+                while(nodoActual.Hermano != null)
+                {
+                    nodoActual = nodoActual.Hermano;
+                }
+                nodoActual.Hermano = new Nodo(dato);
+                return nodoActual.Hermano;
+            }
         }
     }
 }
