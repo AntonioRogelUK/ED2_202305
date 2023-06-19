4c235418b4d7c9cb657651ad1071e45a7a550cee
Yesenia <yeseniamv1995@gmail.com>
2023-06-18 20:01:42 -0700

    Creacion de arbol binario, asi como sus metodos y incluirlo en program.

---
+++ Arboles/ArbolBinario.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arboles
+{
+    internal class ArbolBinario
+    {
+        private readonly NodoBinario raiz;
+        public NodoBinario Raiz { get { return raiz; } }
+
+        public ArbolBinario(int numero)
+        {
+            raiz = new NodoBinario(numero);
+        }
+
+        public void InsertarNodoB(int numero, NodoBinario aux)
+        {
+            if (numero == aux.Numero) return;
+
+            if (numero < aux.Numero)
+            {
+                if (aux.Izq == null)
+                {
+                    aux.Izq = new NodoBinario(numero);
+                }else
+                {
+                    InsertarNodoB(numero, aux.Izq);
+                }
+            }
+
+            if (numero > aux.Numero)
+            {
+                if (aux.Der == null)
+                {
+                    aux.Der = new NodoBinario(numero);
+                }
+                else
+                {
+                    InsertarNodoB(numero, aux.Der);
+                }
+            }  
+        }
+
+        public string ObtenerArbol(NodoBinario nodoInicial = null)
+        {
+            if (nodoInicial == null)
+            {
+                nodoInicial = raiz;
+            }
+
+            int posicion = 0;
+            string datos = string.Empty;
+
+            Recorrer(nodoInicial, ref posicion, ref datos);
+            return datos;
+        }
+
+        private void Recorrer(NodoBinario nodoInicial, ref int posicion, ref string datos)
+        {
+            if (nodoInicial != null)
+            {
+                string dato = nodoInicial.Numero.ToString();
+                int cantidadGuiones = dato.Length + posicion;
+                datos += $"{dato.PadLeft(cantidadGuiones, '-')}\n";
+
+                if (nodoInicial.Izq != null)
+                {
+                    posicion++;
+                    Recorrer(nodoInicial.Izq, ref posicion, ref datos);
+                    posicion--;
+                }
+
+                if (nodoInicial.Der != null)
+                {
+                    posicion++;
+                    Recorrer(nodoInicial.Der, ref posicion, ref datos);
+                    posicion--;
+                }
+            }
+        }
+    }
+}
--- Arboles/Program.cs
+++ Arboles/Program.cs
@@ -10,6 +10,7 @@ namespace Arboles
     {
         static void Main(string[] args)
         {
+            
             ArbolGeneral arbolGeneral = new ArbolGeneral("A");
 
             Nodo nodoB = arbolGeneral.InsertarNodo("B",arbolGeneral.Raiz);
@@ -34,8 +35,31 @@ namespace Arboles
                 Console.WriteLine(renglon);
                 Console.ResetColor();
             }
-            
+         
 
+            ArbolBinario arbolBinario = new ArbolBinario(50);
+
+            arbolBinario.InsertarNodoB(30, arbolBinario.Raiz);
+            arbolBinario.InsertarNodoB(70, arbolBinario.Raiz);
+            arbolBinario.InsertarNodoB(40, arbolBinario.Raiz);
+            arbolBinario.InsertarNodoB(60, arbolBinario.Raiz);
+            arbolBinario.InsertarNodoB(35, arbolBinario.Raiz);
+            arbolBinario.InsertarNodoB(80, arbolBinario.Raiz);
+            arbolBinario.InsertarNodoB(75, arbolBinario.Raiz);
+            arbolBinario.InsertarNodoB(45, arbolBinario.Raiz);
+            arbolBinario.InsertarNodoB(55, arbolBinario.Raiz);
+            arbolBinario.InsertarNodoB(65, arbolBinario.Raiz);
+            arbolBinario.InsertarNodoB(85, arbolBinario.Raiz);
+
+            string arbolB = arbolBinario.ObtenerArbol();
+            string[] renglonesB = arbolB.Split('\n');
+            foreach (string renglon in renglonesB)
+            {
+                int cuentaGuiones = renglon.Count(c => c.ToString() == "-") + 2;
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), cuentaGuiones.ToString());
+                Console.WriteLine(renglon);
+                Console.ResetColor();
+            }
 
             Console.ReadLine();
         }
