ed6119a5af844ed8c919acbd95a4845e38cf66d2
Janosos <126541994+Janosos@users.noreply.github.com>
2023-06-21 22:11:37 -0600

    Clase para los nodos del arbol binario
    La clase nodo usa strings pero no ahora usaremos ints

---
+++ Arboles/NodoABB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arboles
+{
+    internal class NodoABB
+    {
+        // Izq es para los menores y Der para los mayores
+        public int Dato { get; set; }
+        public NodoABB Izq { get; set; }
+        public NodoABB Der { get; set; }
+
+        public NodoABB(int dato)
+        {
+            Dato = dato;
+            Izq = null;
+            Der = null;
+        }
+    }
+}
